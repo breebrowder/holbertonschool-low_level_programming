c38e3acf4eaf99dc8113cb411f44558eeb732c95
breezybrow <browder.bree@gmail.com>
2021-02-03 21:52:40 +0000

    Hello World tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/* main- printing a quote
- *
- * Return: always zero
- */
+/**
+ * main- printing a quote
+ * Return: 0
+*/
 
-int main()
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
