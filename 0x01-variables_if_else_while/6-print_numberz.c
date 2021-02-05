74c852d943a19c23a6ca763f2bcc040a4cf1f293
breezybrow <browder.bree@gmail.com>
2021-02-05 05:48:38 +0000

    Variables if else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main- print numbers 0-9 using only putchar
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i; /* incrementer */
+
+	i = 0;
+
+	while (i <= 0)
+	{
+
+		putchar(i);
+		i++;
+	}
+
+	putchar("\n");
+	return (0);
+
+}
