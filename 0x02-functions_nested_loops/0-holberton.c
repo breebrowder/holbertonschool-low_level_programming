35d8f5f21ec6997061ac32cddd829bef47ade8b2
breezybrow <browder.bree@gmail.com>
2021-02-09 19:02:30 +0000

    Functions nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main- printing holberton followed by a new line
+ *
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	_putchar ('H');
+	_putchar ('o');
+	_putchar ('l');
+	_putchar ('b');
+	_putchar ('e');
+	_putchar ('r');
+	_putchar ('t');
+	_putchar ('o');
+	_putchar ('n');
+	_putchar ('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet- printing alphabet in lowercase
+ *
+ *
+ * 
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+		_putchar(c);
+	_putchar('\n');
+
+}
