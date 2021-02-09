bc6b3bea2268c7d9a031c94cc7cc684db1aa758d
breezybrow <browder.bree@gmail.com>
2021-02-09 19:58:35 +0000

    Functions nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add- returning result of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of addition
+ */
+
+int add(int a, int b)
+{
+
+	return (a + b);
+}
