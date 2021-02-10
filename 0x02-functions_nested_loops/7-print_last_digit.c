5c0d7f766a92d2bee660004064b170cba09e72ba
breezybrow <browder.bree@gmail.com>
2021-02-10 05:30:43 +0000

    Functions nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,11 @@ int print_last_digit(int n)
 {
 	int last;
 
+	printf("Enter a number:");
+	scanf("%d", &n);
+
 	last = n % 10;
-	printf("Last digit: %d", last);
+	printf("Last digit: %d\n", last);
 
 	return (0);
 
