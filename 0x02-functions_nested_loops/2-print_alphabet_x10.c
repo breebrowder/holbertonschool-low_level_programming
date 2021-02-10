92413e8be8d624d9312fea935c2dac9628c0a770
breezybrow <browder.bree@gmail.com>
2021-02-10 00:24:06 +0000

    Functions nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,10 @@ void print_alphabet_x10(void)
 	int i; /* iterator */
 	int ch;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
-
 			_putchar(ch);
-
 		_putchar('\n');
 	}
 }
