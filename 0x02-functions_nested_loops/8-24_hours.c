f21c7422bfed11d9c2c44ca2622ee8257b340d70
breezybrow <browder.bree@gmail.com>
2021-02-11 05:03:01 +0000

    Functions nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer- printing every minute of the day by Jack Bauer
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 24; a++)
+
+		for (b = 0; b < 60; b++)
+
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
+}
