faa4647e2407c4820303b5536a10fcd20cbad1e6
breezybrow <browder.bree@gmail.com>
2021-02-16 21:57:18 +0000

    Pointers/arrays/strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98-  take pointer to int as param, update the value it points to 98
+ * @n: int type
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
