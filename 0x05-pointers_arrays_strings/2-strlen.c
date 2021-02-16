b97c800245b10cbb89a8c1b53a183d1b0ac5ec33
breezybrow <browder.bree@gmail.com>
2021-02-16 23:41:53 +0000

    Pointers/arrays/strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen- returning the length of a string
+ * @s: pointer to string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (i[s] != '\0')
+		i++;
+
+	return (i);
+}
