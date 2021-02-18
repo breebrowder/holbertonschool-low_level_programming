8f986c0962e475e67b55fad40b72ea1fac8c3570
breezybrow <browder.bree@gmail.com>
2021-02-18 04:23:31 +0000

    Pointers/arrays/strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strcpy- copies the string pointed to by src
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	for (; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
