13e6aaa01dad3ba9a2a14c36bd640021540a9fce
breezybrow <browder.bree@gmail.com>
2021-02-18 03:54:29 +0000

    Functions nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- printing 1 through 100 with new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 1;
+
+	while (x <= 100)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
+
+		else if (x % 3 == 0)
+			printf("Fizz");
+
+		else if (x  % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", x);
+
+		if (x != 100)
+			printf(" ");
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
