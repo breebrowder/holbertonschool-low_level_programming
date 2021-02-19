aa19fa4cb115de625821fe297c49b57d6e9954a5
breezybrow <browder.bree@gmail.com>
2021-02-19 04:01:42 +0000

    More pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,16 +15,12 @@ char *leet(char *str)
 	{
 		if (str[leet] == 'a' || str[leet] == 'A')
 			str[leet] = '4';
-
 		while (str[leet] == 'e' || str[leet] == 'E')
 			str[leet] = '3';
-
 		while (str[leet] == 'o' || str[leet] == 'O')
 			str[leet] = '0';
-
 		while (str[leet] == 't' || str[leet] == 'T')
 			str[leet] = '7';
-
 		while (str[leet] == 'l' || str[leet] == 'L')
 			str[leet] = '1';
 		leet++;
