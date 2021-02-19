b40b6faedb40b6df991e3551359ff936fc6037d2
breezybrow <browder.bree@gmail.com>
2021-02-19 20:37:25 +0000

    Personal header

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -9,7 +9,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *upper);
 char *cap_string(char *s);
-int _strlen(char *s);
 char *leet(char *str);
 char *rot13(char *s);
 
