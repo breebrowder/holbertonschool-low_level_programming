88dde19df25ababbb55e0df7e04822b85d31a9be
breezybrow <browder.bree@gmail.com>
2021-02-19 00:40:38 +0000

    More pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 /* dest, src, and n have already been delcared */
 
 	for (; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
+
 	for (i = 0; i < n; i++)
-	{
 		dest[i] = '\0';
-	}
+
 	return (dest);
 }
