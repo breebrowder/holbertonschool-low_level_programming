1c25feba13649aa963f037202535bd663071fc90
breezybrow <browder.bree@gmail.com>
2021-02-28 17:57:25 +0000

    Argc/Argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- printing all arguments recieved, followed by new line
+ * @argc: argument count
+ * @argv: argument vector (passed as an array of strings)
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i; /* iterator through array of strings in argv */
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
