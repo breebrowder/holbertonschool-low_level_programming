d5f3bfcd4fd89b4a38076c0ffff3a19d33ab916e
breezybrow <browder.bree@gmail.com>
2021-02-28 17:07:27 +0000

    Argc/Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- printing program name, followed by new line
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	(void)argc;
+
+	printf("./mynameis\n");
+	return (0);
+
+}
