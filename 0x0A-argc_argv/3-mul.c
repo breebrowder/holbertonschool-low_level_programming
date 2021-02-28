3976c1bd9291175fe5c8a5742d8cecd875be391e
breezybrow <browder.bree@gmail.com>
2021-02-28 23:37:48 +0000

    Argc/Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,14 +16,12 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-
-                printf("Error\n");
-		return (EXIT_FAILURE); /* same as 1; from stdlib */
+		printf("Error\n");
+		return (EXIT_FAILURE); /* unsuccessful execution */
 	}
-
 	if (argv[1] && argv[2]) /* 1 & 2 for both indexes */
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
-	return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS); /* successful execution */
 }
