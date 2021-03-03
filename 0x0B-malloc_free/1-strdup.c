761b14ff96ab1f324881a74d1cd2e6ab201a9f46
breezybrow <browder.bree@gmail.com>
2021-03-03 04:50:08 +0000

    Memory allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup- returns a pointer to a new space in memory, contains a copy of str
+ * @str: string
+ *
+ * Return: pointer to duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int x = 0; /* iterator for str */
+	int y = 0;
+
+	if (str == '\0')
+		return ('\0'); /* condition */
+
+	for (; str[x] != '\0'; x++)
+		continue;
+
+	dup = malloc(x * sizeof(*dup) + 1);
+	while (y < x)
+	{
+		dup[y] = str[y];
+		y++;
+	}
+	if (dup == '\0')
+		return ('\0'); /* condition */
+
+	return (dup); /* condition */
+}
