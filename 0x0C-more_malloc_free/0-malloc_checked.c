ba111b1f72f9db7b947c5d9e5a5d606a84e12836
breezybrow <browder.bree@gmail.com>
2021-03-05 01:48:47 +0000

    More malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked- allocates memory using malloc
+ * @b: sizeof
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *allocatedmemory;
+
+	allocatedmemory = malloc(b);
+
+	if (allocatedmemory == '\0') /* condition if it fails */
+		exit(98); /* process termination with status value */
+
+	return (allocatedmemory); /* condition */
+}
