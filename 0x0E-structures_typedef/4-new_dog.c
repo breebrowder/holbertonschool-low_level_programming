80a9ed5c6910f0dd761aee3e029cb2396ae01d4b
breezybrow <browder.bree@gmail.com>
2021-03-09 05:55:02 +0000

    partial credit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog- creates a new dog
+ * @name: pointer to name
+ * @age: type int age
+ * @owner: pointer to owner
+ *
+ * Return: NULL if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d->name == NULL)
+		return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+/**
+ * _strcpy- copies the string pointed to by src
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	for (; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[x] = '\0';
+
+	return (dest);
+}
