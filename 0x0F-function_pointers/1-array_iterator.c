9e8acf4807258fcfed48bad7e397dfea231ac474
breezybrow <browder.bree@gmail.com>
2021-03-10 23:49:58 +0000

    Func pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator- executes a function given as parameter in an array
+ * @array:arrays of ints
+ * @size: size of the array
+ * @action: function pointer
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t bree = 0; /* iterator for array */
+
+	for (; bree < size; bree++)
+	action(array[bree]);
+}
