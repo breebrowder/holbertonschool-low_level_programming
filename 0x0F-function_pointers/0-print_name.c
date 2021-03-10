f186fc2e6d9a1fcdd61e19354b9ae758e59a5536
breezybrow <browder.bree@gmail.com>
2021-03-10 23:34:12 +0000

    Func pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name- prints name from function pointer
+ * @name: character string
+ * @f: function pointer; takes a string
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
