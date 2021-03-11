ce164247e3785aa098dab80ff6cdbadb0268f0db
breezybrow <browder.bree@gmail.com>
2021-03-11 00:34:42 +0000

    Func pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "function_pointers.h"
+
+/**
+ * op_add- returns sum of two ints
+ * @a: type int
+ * @b: type int
+ *
+ * Return: int result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub- returns the difference of two ints
+ * @a: type int
+ * @b: type int
+ *
+ * Return: int result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- returns the product of two ints
+ * @a: type int
+ * @b: type int
+ *
+ * Return: int result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div- returns result of division of two ints
+ * @a: type int
+ * @b: type int
+ *
+ * Return: int result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- returns the remainder of the division of two ints
+ * @a: type int
+ * @b: type int
+ *
+ * Return: int result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
