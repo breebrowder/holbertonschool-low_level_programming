df2705a0f002f6054a27cf90b72733f4c348d7a7
breezybrow <browder.bree@gmail.com>
2021-03-11 00:02:34 +0000

    Func pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t bree = 0; /* iterator for array */
 
+	if (array != '\0' && action != '\0')
+
 	for (; bree < size; bree++)
-	action(array[bree]);
+	{
+		action(array[bree]);
+	}
 }
