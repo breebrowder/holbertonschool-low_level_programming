c726606bc1d188ee99fbb9332663fb65c6af95a2
breezybrow <browder.bree@gmail.com>
2021-03-24 01:16:44 +0000

    Linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2- free a list
+ * @head: double pointer to head
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *freelist;
+
+	if (head == NULL)
+		return; /* condition: true */
+
+	*head = NULL;
+	while (*head != NULL)
+	{
+		freelist = *head;
+		*head = freelist->next;
+		free(freelist);
+	}
+}
