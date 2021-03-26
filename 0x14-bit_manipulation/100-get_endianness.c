3918d790e7fab87145fb57e170ed90502afbe8b0
breezybrow <browder.bree@gmail.com>
2021-03-26 01:04:40 +0000

    Adv task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * get_endianness- function that checks the endianness
+ *
+ * Return: 0 if big, 1 if little
+ */
+
+int get_endianness(void)
+{
+	int j;
+	char *k;
+
+	j = 1;
+	k = (char*)&j;
+
+	return (*k);
+}
