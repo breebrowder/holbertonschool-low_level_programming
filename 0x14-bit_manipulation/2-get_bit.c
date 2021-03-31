867adcd8dd002884c3fc5d97ec9f87e5fd12890b
breezybrow <browder.bree@gmail.com>
2021-03-31 00:09:54 +0000

    Bit Manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * get_bit- function that returns the value of a bit at given index
+ * @n: number or bit
+ * @index: index position
+ *
+ * Return: the value of the bit at index or -1 if an error occurred
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+        unsigned long int bitnum;
+
+        bitnum = n;
+
+        if (index > 64) /* sizeof unsginedlongint times 8 */
+                return (-1); /*error occured */
+
+        return (bitnum >> index & 1);
+}
