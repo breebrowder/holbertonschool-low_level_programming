12878301300a2f4fb71f69aab5341f2b799fa784
breezybrow <browder.bree@gmail.com>
2021-03-31 04:46:08 +0000

    saving progress

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,8 +10,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t mainfile, readpermfile, writepermfile;
+	char *buffer;
 
+	if (filename == NULL)
+		return (0);
 
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+		return (0);
 
+	mainfile = open(filename, O_RDONLY);
+	if (mainfile == -1)
+		return (0); /* condition: file cannot be open or read */
+
+	readpermfile = read(mainfile, buffer, letters);
+	if (readpermfile == -1)
+		return (0); /* condition: file cannot be open or read */
+
+	writepermfile = write(STDOUT_FILENO, buffer, readpermfile);
+	if (writepermfile == -1)
+		return (0); /* condition: file cannot be open or read */
+
+	free(buffer); /* for every malloc you have to free */
+	close(mainfile);
+	return (writepermfile);
 
 }
