3903c5a917c7202f979afbe81e4bef3783c64a50
breezybrow <browder.bree@gmail.com>
2021-03-31 15:18:43 +0000

    Retry

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,20 +34,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1); /* condition: failure */
 
-	if (text_content == NULL)
-		text_content = "";
-
 	if (text_content != NULL)
 		realstrlen = _strlen(text_content);
 
-	mainfile = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	mainfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (mainfile == -1)
 		return (-1); /* condition: failure */
 
 	writepermfile = write(mainfile, text_content, realstrlen);
+	close(mainfile);
+
 	if (writepermfile == -1)
 		return (-1); /* condition: failure */
 
-	close(mainfile);
 	return (1); /* success */
 }
