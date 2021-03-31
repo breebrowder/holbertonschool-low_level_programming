a7a23fe700fe3707a767f3e34fa05d56496802e4
breezybrow <browder.bree@gmail.com>
2021-03-31 04:58:59 +0000

    saving progress

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,6 +34,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1); /* condition: failure */
 
+	if (text_content == NULL)
+		text_content = "";
+
 	if (text_content != NULL)
 		realstrlen = _strlen(text_content);
 
