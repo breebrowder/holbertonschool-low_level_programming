16d4baaee87fbb0708b9aad64970109faa1c948c
breezybrow <browder.bree@gmail.com>
2021-07-29 20:52:56 +0000

    Task 4

---
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_get- function that retrieves a value associated with a key
+ * @ht: pointer to hash table
+ * @key: the key
+ * Return: value associated with the element, or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+{
+        unsigned long int index;
+	hash_node_t *e = NULL;
+
+	/* e is our element */
+
+	if (ht == NULL || key == NULL)
+		return (NULL); /* condition */
+
+	if (e == NULL)
+                return (NULL); /* condition */
+
+	index = 0;
+
+	index = key_index((unsigned char *) key, ht->size);
+	e = ht->array[index];
+
+	while (strcmp(key, e->key) != 0)
+		e = e->next;
+
+	return (e->value); /* return value assoc with element */
+}
