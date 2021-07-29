44ba301c42b2ca437af4bf66455014565ce068a0
breezybrow <browder.bree@gmail.com>
2021-07-29 20:26:24 +0000

    Repush

--- 0x00-hash_tables/2-key_index.c
+++ 0x00-hash_tables/2-key_index.c
@@ -4,7 +4,7 @@
  * key_index - function that gives you the index of a key
  * @key: pointer to key
  * @size: size of hash table
- * Return: index at which the key/value pair is stored in the array of the hash table
+ * Return: index at which the key pair is stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
