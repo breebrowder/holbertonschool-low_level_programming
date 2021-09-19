55b15085fbcb1968871cc79b4e8768b8a8ba79c3
breezybrow <browder.bree@gmail.com>
2021-09-19 16:33:50 +0000

    Pushing skeleton files until completed

---
+++ 0x02-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search- function that searches for a value in an array of integers
+ * USE LINEAR SEARCH ALGORITHM
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: the first index where the value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+
+
+
+
+
+
+
+
+}
---
+++ 0x02-search_algorithms/1-binary.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * binary_search- function that searches for a value in a sorted array of ints
+ * USE BINARY SEARCH ALGORITHM
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: the first index where the value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+
+
+
+
+
+
+
+
+}
