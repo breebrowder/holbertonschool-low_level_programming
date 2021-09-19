3119df01ebbc8c489cbefed1b1f9877b364fe4ef
breezybrow <browder.bree@gmail.com>
2021-09-19 16:20:21 +0000

    Push README.md, header, and text files

---
+++ 0x02-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+
+/* Task 0 */
+int linear_search(int *array, size_t size, int value);
+
+/* Task 1 */
+int binary_search(int *array, size_t size, int value);
+
+#endif /*SEARCH_H */
