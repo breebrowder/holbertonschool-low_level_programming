542c69be23b19ec61d22121b8632e351eb2d180a
breezybrow <browder.bree@gmail.com>
2021-09-20 21:55:59 +0000

    Task 1: Binary search

--- 0x02-search_algorithms/1-binary.c
+++ 0x02-search_algorithms/1-binary.c
@@ -14,11 +14,30 @@
 int binary_search(int *array, size_t size, int value)
 {
 
+	size_t idx, lowest, highest;
 
+	if (array == NULL)
+		return (-1); /* condition */
 
+	for (lowest = 0, highest = size - 1; highest >= lowest;)
+	{
+		idx = lowest;
 
+		printf("Searching in array: ");
+		for (; idx < highest; idx++)
+			printf("%d, ", array[idx]);
+		printf("%d\n", array[idx]);
 
+		idx = lowest + (highest - lowest) / 2;
 
+		if (array[idx] == value)
+			return (idx);
 
+		if (array[idx] > value)
+			highest = idx - 1;
+		else
+			lowest = idx + 1;
+	}
 
+	return (-1); /* failure */
 }
